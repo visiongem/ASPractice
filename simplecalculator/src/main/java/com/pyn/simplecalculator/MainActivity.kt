982be0b8aae6e678abee017a3f2716b23dadcbf4
pengyanni <393507488@qq.com>
2020-05-27 18:47:52 +0800

    1. simple caculator and repair bug 2. without fully tested, might with some unkonwn bugs.( hhhh ~~ )

--- simplecalculator/src/main/java/com/pyn/simplecalculator/MainActivity.kt
+++ simplecalculator/src/main/java/com/pyn/simplecalculator/MainActivity.kt
@@ -10,6 +10,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.pyn.simplecalculator.databinding.ActivityMainBinding
 
+/**
+ * 简易的计算机，规则定制简单点
+ */
 class MainActivity : AppCompatActivity() {
 
     private val TAG: String = this.javaClass.simpleName
@@ -52,16 +55,20 @@ class MainActivity : AppCompatActivity() {
         val resId = view.id
         // 开根号控件和其他按钮不一样，做个输入区分
         var inputText = if (resId == R.id.ib_sqrt) {
-            "√"
+            resources.getString(R.string.sqrt)
         } else {
             (view as TextView).text.toString()
+        }
+
+        if (operator == resources.getString(R.string.equal)) {
+            clear()
         }
 
         when (resId) {
             R.id.btn_clear -> clear()
             R.id.btn_cancel -> {
                 // 无操作符，则表示逐位取消前一个操作数
-                if (operator.equals("")) {
+                if (TextUtils.isEmpty(operator)) {
                     if (firstNum.length == 1) {
                         firstNum = "0"
                     } else {
@@ -71,31 +78,62 @@ class MainActivity : AppCompatActivity() {
                     mBinding.tvResult.text = showText
                 } else {
                     // 有操作符，则表示逐位取消后一个操作数
-                    if (nextNum.length == 1) {
-                        nextNum = ""
+                    if (operator == resources.getString(R.string.sqrt)) {
+                        // 根号的情况
+                        if (showText.length == 1) {
+                            clear()
+                        } else {
+                            firstNum = firstNum.substring(0, firstNum.length - 1)
+                            showText = "√$firstNum"
+                            mBinding.tvResult.text = showText
+                        }
                     } else {
-                        nextNum = nextNum.substring(0, nextNum.length - 1)
+                        // 不是根号的情况
+                        if (nextNum.length > 1) {
+                            nextNum = nextNum.substring(0, nextNum.length - 1)
+                        } else if (nextNum.length == 1) {
+                            nextNum = ""
+                        } else {
+                            operator = ""
+                        }
+                        showText = showText.substring(0, showText.length - 1)
+                        mBinding.tvResult.text = showText
                     }
-                    showText = showText.substring(0, showText.length - 1)
-                    mBinding.tvResult.text = showText
                 }
             }
             R.id.btn_equal -> {
                 if (!TextUtils.isEmpty(operator)) {
-                    if (caculate()) {
-                        showText = showText + inputText + result
+                    if (operator != resources.getString(R.string.sqrt)) {
+                        // + - * /
+                        if (caculate()) {
+                            showText = showText + inputText + result
+                        } else {
+                            clear();
+                            return
+                        }
                     } else {
-                        return
+                        // 开根号
+                        if (firstNum != "0") {
+                            result = Math.sqrt(firstNum.toDouble()).toString()
+                            showText = showText + inputText + result
+                        } else {
+                            showText = showText + "0=0"
+                        }
                     }
                 }
                 operator = inputText
+                mBinding.tvResult.text = showText
             }
             R.id.ib_sqrt -> {
-                if (TextUtils.isEmpty(operator)) {
-                    result = Math.sqrt(firstNum.toDouble()).toString()
 
+                // 开根号必须放第一位，就这规定。哈哈~
+                if (showText == "0") {
+                    operator = inputText
+                    showText = operator
+                    mBinding.tvResult.text = showText
                 } else {
-
+                    Toast.makeText(this, "计算开根号请先输入根号，再输入数字", Toast.LENGTH_SHORT).show()
+                    return
                 }
             }
 
@@ -112,17 +150,72 @@ class MainActivity : AppCompatActivity() {
                     showText = "0"
                 }
                 if (TextUtils.isEmpty(operator)) {
-                    if (firstNum.contains(".") && inputText.equals(".")) {
+                    // 包含小数点的情况
+                    if (firstNum.contains(resources.getString(R.string.dot)) && inputText == resources.getString(
+                            R.string.dot
+                        )
+                    ) {
                         return
                     }
-                    firstNum += inputText
-                } else {
-                    if (nextNum.contains(".") && inputText.equals(".")) {
-                        return; // 一个数字不能有两个小数点
+
+                    // 初始状态的情况，输入0，显示0的情况
+                    if (inputText == "0" && showText == "0") {
+                        return
                     }
-                    nextNum += inputText
+                    if (firstNum == "0") {
+                        if (inputText == resources.getString(R.string.dot)) {
+                            firstNum = "0."
+                        } else {
+                            firstNum = inputText
+                        }
+                    } else {
+                        firstNum += inputText
+                    }
+                    showText = firstNum
+                } else {
+                    if (operator != resources.getString(R.string.sqrt)) {
+                        if (nextNum.contains(resources.getString(R.string.dot)) && inputText == resources.getString(
+                                R.string.dot
+                            )
+                        ) {
+                            return; // 一个数字不能有两个小数点
+                        }
+                        // 禁止输入两个0的情况
+                        if (nextNum == "0" && inputText == "0") {
+                            return
+                        }
+
+                        if (inputText == resources.getString(R.string.dot) && (nextNum == "")) {
+                            nextNum = "0."
+                        } else {
+                            nextNum += inputText
+                        }
+                        showText = firstNum + operator + nextNum
+                    } else {
+                        // 包含小数点的情况
+                        if (firstNum.contains(resources.getString(R.string.dot)) && inputText == resources.getString(
+                                R.string.dot
+                            )
+                        ) {
+                            return
+                        }
+
+                        // 初始状态的情况，输入0，显示0的情况
+                        if (inputText == "0" && firstNum == "0" && showText == "√0") {
+                            return
+                        }
+                        if (firstNum == "0") {
+                            if (inputText == resources.getString(R.string.dot)) {
+                                firstNum = "0."
+                            } else {
+                                firstNum = inputText
+                            }
+                        } else {
+                            firstNum += inputText
+                        }
+                        showText = "√" + firstNum
+                    }
                 }
-                showText += inputText
                 mBinding.tvResult.text = showText
             }
         }
@@ -143,13 +236,11 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "被除数不能为零", Toast.LENGTH_SHORT).show()
                     return false
                 } else {
-                    result
+                    result = Arith.div(firstNum, nextNum)
                 }
             }
         }
         Log.d(TAG, "result = $result")
-        firstNum = result
-        nextNum = ""
         return true
     }
 
@@ -157,10 +248,10 @@ class MainActivity : AppCompatActivity() {
      * 清空并初始化
      */
     private fun clear() {
-        showText = ""
-        mBinding.tvResult.text = ""
+        showText = "0"
+        mBinding.tvResult.text = "0"
         operator = ""
-        firstNum = ""
+        firstNum = "0"
         nextNum = ""
         result = ""
     }
