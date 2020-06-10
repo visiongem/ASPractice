91b31dd35fccb64c091e7f1ac71055f24c29fe7e
pengyanni <393507488@qq.com>
2020-06-10 17:35:35 +0800

    commit login UI

--- login/src/main/java/com/pyn/login/LoginActivity.kt
+++ login/src/main/java/com/pyn/login/LoginActivity.kt
@@ -2,11 +2,15 @@ package com.pyn.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pyn.login.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var mBinding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        mBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
     }
 }
