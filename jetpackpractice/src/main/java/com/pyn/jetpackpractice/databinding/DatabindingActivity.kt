ae14537dbe96831751112e01ace6821719a4f0fb
yane <pengyanni@fcsc.com>
2020-11-04 13:52:09 +0800

    databing user's BindingAdapter

--- jetpackpractice/src/main/java/com/pyn/jetpackpractice/databinding/DatabindingActivity.kt
+++ jetpackpractice/src/main/java/com/pyn/jetpackpractice/databinding/DatabindingActivity.kt
@@ -2,26 +2,42 @@ package com.pyn.jetpackpractice.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.pyn.jetpackpractice.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class DatabindingActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDatabindingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        var book = Book("title","author", 1)
+        var book = Book("title", "author", 1)
 
-        val binding: ActivityDatabindingBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_databinding)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_databinding)
 
         binding.book = book
 
         binding.eventHandler = EventHandleListener(this)
 
-        binding.networkImage = "https://static01.imgkr.com/temp/a5f0ff2e7ab741ffa49504c097b56fac.jpg"
+        binding.networkImage =
+            "https://static01.imgkr.com/temp/a5f0ff2e7ab741ffa49504c097b56fac.jpg"
 
         binding.localImage = R.drawable.ic_launcher_background
 
+        binding.clickHandler = ClickHandler()
+        binding.imagePadding = 40
+
+    }
+
+    inner class ClickHandler {
+
+        fun onClick(view: View) {
+            Log.d("ClickHandler","padding")
+            binding.imagePadding = 100
+        }
     }
 }
--- jetpackpractice/src/main/java/com/pyn/jetpackpractice/databinding/ImageViewBindingAdapter.java
+++ jetpackpractice/src/main/java/com/pyn/jetpackpractice/databinding/ImageViewBindingAdapter.java
@@ -1,7 +1,8 @@
 package com.pyn.jetpackpractice.databinding;
 
-import android.graphics.Color;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.databinding.BindingAdapter;
@@ -11,20 +12,28 @@ import com.squareup.picasso.Picasso;
 
 public class ImageViewBindingAdapter {
 
-    @BindingAdapter("image")
-    public static void setImage(ImageView imageView, String imageUrl) {
+    @BindingAdapter(value = {"imageUrl", "defaultImageResource"}, requireAll = false)
+    public static void setImage(ImageView imageView, String imageUrl, int imageResource) {
         if (!TextUtils.isEmpty(imageUrl)) {
             Picasso.get().load(imageUrl)
                     .placeholder(R.drawable.ic_launcher_background)
                     .error(R.drawable.ic_launcher_foreground)
                     .into(imageView);
-        }else {
-            imageView.setBackgroundColor(Color.DKGRAY);
+        } else {
+            imageView.setBackgroundColor(imageResource);
         }
     }
 
     @BindingAdapter("image")
-    public static void setImage(ImageView imageView, int imageResource){
+    public static void setImage(ImageView imageView, int imageResource) {
         imageView.setImageResource(imageResource);
+    }
+
+    @BindingAdapter("padding")
+    public static void setPadding(View view, int oldPadding, int newPadding) {
+        Log.d("ImageViewBindingAdapter", "oldPadding = " + oldPadding + "  newPadding = " + newPadding);
+        if (oldPadding != newPadding) {
+            view.setPadding(newPadding, newPadding, newPadding, newPadding);
+        }
     }
 }
