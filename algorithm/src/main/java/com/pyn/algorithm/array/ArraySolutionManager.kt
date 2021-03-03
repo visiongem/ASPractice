30175e3ea1164f9570f0c52b94089dcdaad25f0c
yane <pengyanni@fcsc.com>
2021-03-03 09:26:47 +0800

    买卖股票的最佳时机 II(贪心算法)

--- algorithm/src/main/java/com/pyn/algorithm/array/ArraySolutionManager.kt
+++ algorithm/src/main/java/com/pyn/algorithm/array/ArraySolutionManager.kt
@@ -22,4 +22,22 @@ object ArraySolutionManager {
         }
         return length + 1
     }
+
+    /**
+     * 贪心算法
+     * 买卖股票的最佳时机 II
+     * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+     */
+    fun maxProfit(prices: IntArray): Int {
+        if (prices.isEmpty()) {
+            return 0
+        }
+
+        var ans = 0;
+        var n = prices.size
+        for (i in 1 until n) {
+            ans += Math.max(0, prices[i] - prices[i - 1]);
+        }
+        return ans
+    }
 }
