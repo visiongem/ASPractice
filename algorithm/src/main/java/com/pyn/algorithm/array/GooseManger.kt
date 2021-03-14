8d279557bce1be5df32a99f7c8ab71c8f5c8b526
visiongem <393507488@qq.com>
2021-03-14 11:38:53 +0800

    多数元素

--- algorithm/src/main/java/com/pyn/algorithm/array/GooseManger.kt
+++ algorithm/src/main/java/com/pyn/algorithm/array/GooseManger.kt
@@ -171,6 +171,26 @@ object GooseManger {
         }
         return result
     }
+
+    /**
+     * 反转字符串
+     * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+     * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+     * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+     */
+    fun reverseString(s: CharArray): Unit {
+
+        if (s.size == 1) {
+            return
+        }
+
+        // 交换数字方式
+        for (index in 0 until s.size / 2) {
+            var temp = s[index]
+            s[index] = s[s.size - index - 1]
+            s[s.size - index - 1] = temp
+        }
+    }
 }
 
 class ListNode(var value: Int) {
