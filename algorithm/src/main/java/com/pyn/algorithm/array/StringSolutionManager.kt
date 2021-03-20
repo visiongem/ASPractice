e86e0e57035f2b389799b09ab60bf9a6b6089efc
visiongem <393507488@qq.com>
2021-03-20 14:16:10 +0800

    验证回文串

--- algorithm/src/main/java/com/pyn/algorithm/array/StringSolutionManager.kt
+++ algorithm/src/main/java/com/pyn/algorithm/array/StringSolutionManager.kt
@@ -97,4 +97,29 @@ object StringSolutionManager {
 
         return schar.contentEquals(tchar)
     }
+
+    /**
+     * 5 验证回文串
+     * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+     * 说明：本题中，我们将空字符串定义为有效的回文串。
+     */
+    fun isPalindrome(s: String): Boolean {
+
+        var left = 0
+        var right = s.length - 1
+        while (left < right) {
+
+            while (left < right && !Character.isLetterOrDigit(s[left]))
+                left++
+            while (left < right && !Character.isLetterOrDigit(s[right]))
+                right--
+
+            if (Character.toLowerCase(s[left]) != Character.toLowerCase(s[right]))
+                return false
+            left++
+            right--
+        }
+        return true
+
+    }
 }
