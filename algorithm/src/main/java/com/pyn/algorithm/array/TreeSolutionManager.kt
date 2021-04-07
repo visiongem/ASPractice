9676ce8b64f8b45ecfcbe6e3c58e5327470e29ce
visiongem <393507488@qq.com>
2021-04-07 11:11:21 +0800

    将有序数组转换为二叉搜索树(copy ans)

--- algorithm/src/main/java/com/pyn/algorithm/array/TreeSolutionManager.kt
+++ algorithm/src/main/java/com/pyn/algorithm/array/TreeSolutionManager.kt
@@ -162,7 +162,21 @@ object TreeSolutionManager {
      * 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
      */
     fun sortedArrayToBST(nums: IntArray): TreeNode? {
+        // 中序遍历，总是选择中间位置左边的数字作为根节点
+        return helper(nums, 0, nums.size - 1)
+    }
 
+    fun helper(nums: IntArray, left: Int, right: Int): TreeNode? {
+        if (left > right) {
+            return null
+        }
+        // 总是选择中间位置左边的数字作为根结点
+        var mid = (left + right) / 2
+
+        var root = TreeNode(nums[mid])
+        root.left = helper(nums, left, mid - 1)
+        root.right = helper(nums, mid + 1, right)
+        return root
     }
 
     class TreeNode(var `val`: Int) {
